24a4837a8cffb549341b99936f488fd26b851860
TungPT04 <tunggod24@gmail.com>
2024-01-30 09:17:46 +0700

    Geo

---
+++ src/main/java/com/mycompanycom/mycompany/geographic/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompanycom.mycompany.geographic;
+import controller.ManageEastAsiaCountries;
+/**
+ *
+ * @author tung
+ */
+public class Main {
+    public static void main(String[] args) {
+        new ManageEastAsiaCountries().run();
+    }
+}
